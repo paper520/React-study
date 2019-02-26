f8f31a52b246408f02ac7f74a28432cfc2553ede
心叶 <yelloxing@gmail.com>
2019-02-26 16:57:28 +0800

    添加基本样式文件

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import '../node_modules/easycss-core/build/easycss.min.css';
 
 const div = (
   <div>Hello JSX!</div>
--- webpack.common.js
+++ webpack.common.js
@@ -6,6 +6,9 @@ module.exports = {
   },
   module: {
     rules: [{
+      test: /\.css$/,
+      use: ['style-loader','css-loader']
+    }, {
       test: /\.(js|jsx)$/,
       use: 'babel-loader',
       exclude: /node_modules/
