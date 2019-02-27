8de78cbf30225451763fec8adea72e80d854f08e
心叶 <yelloxing@gmail.com>
2019-02-27 10:28:13 +0800

    JSX基本写法

--- src/containers/JSX.jsx
+++ src/containers/JSX.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
 
+// 点表示法用于JSX类型
+const Demo1 = {
+  MyComponent: function (props) {
+    return <div>传递进来的数据是：msg={props.msg},val={props.val}</div>;
+  }
+};
+
+// 展开属性
+// [下面二种写法是一样的效果]
+function Demo2_App1() {
+  return <div name='名称1' className='App1'>app1</div>;
+}
+function Demo2_App2() {
+  const json = { name: "名称2", className: 'App2' };
+  return <div {...json}>App2</div>;
+}
+
+// 表达式中迭代
+function Demo3() {
+  const data = ["数据一", "数据二", "数据三"];
+  return (<ul>
+    {data.map((val, index) => <li key={index}>{val}</li>)}
+  </ul>);
+}
+
 export default class JSX extends React.Component {
   render() {
     return (
-      <div>
-        <a>jsx学习</a>
+      // 因为是jsx代码中，因此内联样式需要这样赋值
+      <div style={{ padding: '0.3rem' }}>
+        <h3>jsx学习</h3>
+        <hr />
+        <Demo1.MyComponent msg='测试数据' val={1 + 2} />
+        <hr />
+        <Demo2_App1 />
+        <Demo2_App2 />
+        <hr />
+        <Demo3 />
+        <hr />
       </div>
-    )
+    );
   }
 };
--- src/containers/menu.jsx
+++ src/containers/menu.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 
+const liStyle = {
+  display: 'inline-block',
+  margin: ".3rem"
+};
+
+const aStyle = {
+  backgroundColor: '#2196F3',
+  color: "white",
+  lineHeight: "3em",
+  padding: "0 .3rem"
+};
+
 export default class menu extends React.Component {
   render() {
+
+    const menulist = [
+      ["jsx", "JSX基本写法"]
+    ];
+
     return (
       <ul>
-        <li>
-          <a href="#/jsx">JSX</a>
-        </li>
+        {menulist.map((value, key) => (
+          <li key={key} style={liStyle}>
+            <a href={"#/" + value[0]} style={aStyle}>{value[1]}</a>
+          </li>))}
       </ul>
-    )
+    );
   }
 };
